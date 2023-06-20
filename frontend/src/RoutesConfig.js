15cdce59c66523a01952bfc2c33e52f220625d4f
Lukas Volks <professionalvolks@gmail.com>
2023-06-20 14:27:11 -0300

    Alterações front end

--- frontend/src/RoutesConfig.js
+++ frontend/src/RoutesConfig.js
@@ -3,14 +3,20 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Cliente from './pages/Cliente';
 import Pedido from './pages/Pedido';
+import Home from './pages/home'
+import Planos from './pages/planos'
+import Sobre from './pages/sobre'
 
 export default function RoutesConfig() {
     return (
         <Routes>
-            <Route path='/' element={<Pedido />} />
+            <Route path='/' element={<Home />} />
             <Route path='cliente' element={<Cliente />} />
             <Route path='login' element={<Login />} />
+            <Route path='pedido' element={<Pedido />} />
             <Route path='*' element={<h1> 404 - Página Não Encontrada!</h1>} />
+            <Route path='planos' element={<Planos />} />
+            <Route path='sobre' element={<Sobre />} />
         </Routes>
     );
 }
