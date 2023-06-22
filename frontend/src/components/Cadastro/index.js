a60e28bf04ed4cb7233ac545f0cb7fffc2b98f88
Lukas Volks <professionalvolks@gmail.com>
2023-06-21 22:39:52 -0300

    Alterações na home e página detalhes adicionada aos produtos

--- frontend/src/components/Cadastro/index.js
+++ frontend/src/components/Cadastro/index.js
@@ -146,7 +146,6 @@ export default function Cadastro() {
                   <input
                     type="file"
                     accept="image/*"
-                    value={imagem}
                     className="form-control"
                     onChange={handleSelectImg}
                   />
---
+++ frontend/src/pages/detalhes.js
@@ -0,0 +1,51 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import "./detalhes.css";
+
+export default function Detalhes() {
+    const [produto, setProduto] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const { id} = useParams();
+
+    useEffect(() => {
+
+        fetch(`https://my-json-server.typicode.com/lvolks/demo/produtos/${id}`)
+            .then(response => response.json())          
+            .then(data => {
+                setProduto(data);
+                setLoading(false); 
+            })
+            .catch(err => console.error(err))
+    }, []);
+
+        
+        return (
+            <div className="container">
+            {loading ? (
+              <h1 className="display-6">Carregando...</h1>
+            ) : (
+              <>
+                {Object.keys(produto).length === 0 ? (
+                  <h1 className="display-6">Nenhum filme encontrado.</h1>
+                ) : (
+                  <div className="row">
+                    <div className="col">
+                      <div className="card" style={{ maxWidth: "500px" }}>
+                        <img src={produto.imagem} alt={produto.nome} className="card-img-top" />
+                      </div>
+                    </div>
+                    <div className="col">
+                      <div className="card mx-auto p-3">
+                        <h1 className="text-center">{produto.nome}</h1>
+                        <p className="text-center">Preço do produto: {produto.preço}R$</p>
+                        <p className="text-center">Descrição do produto: {produto.descrição}</p>
+                      </div>
+                    </div>
+                  </div>
+                )}
+              </>
+            )}
+          </div>
+        );
+                
+}
