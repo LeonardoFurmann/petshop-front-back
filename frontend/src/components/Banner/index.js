948817f3f45e07d29e7cd082a7fb7d4c9e2e8f7e
RodBerw <rodolfo.b.liberado@gmail.com>
2023-06-23 17:53:11 -0300

    Alteração do design das páginas

--- frontend/src/components/Banner/index.js
+++ frontend/src/components/Banner/index.js
@@ -1,10 +1,27 @@
+import "./Banner.css"
+
+
 export default function Banner(){
 
-    const imagePath = process.env.PUBLIC_URL + '/';
+    const bannerImgPath = process.env.PUBLIC_URL + '/banner-img.jpg';
+    const logoImgPath = process.env.PUBLIC_URL + '/the-petshop-logo-transparent.png';
+    const acessorios = document.getElementsByClassName("acessorios")[0];
+    const brinquedos = document.getElementsByClassName("brinquedos")[0];
+    const racoes = document.getElementsByClassName("racoes")[0];
 
     return(
-        <div className="Banner">
-            <img></img>
+        <div>
+            <img className="banner" src={bannerImgPath}></img>
+            <div className="slogan">
+                <h1 className="slogan-1">The PetShop</h1>
+                <h3 className="slogan-2">Os melhores produtos para o seu pet, quando e onde você estiver!</h3>
+            </div>
+            <div className="banner-container">
+                <div className="item 1">Acessórios</div>
+                <div className="item 2">Brinquedos</div>
+                <div className="item 3">Rações</div>
+            </div>
+            <img className="logo" src={logoImgPath}></img>
         </div>
     )
 }
