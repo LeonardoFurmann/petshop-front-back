8f608f10678b277a1e19f7746322de1a01735215
RodBerw <rodolfo.b.liberado@gmail.com>
2023-06-23 14:04:19 -0300

    alterações antigas 2

---
+++ frontend/src/components/Banner/index.js
@@ -0,0 +1,10 @@
+export default function Banner(){
+
+    const imagePath = process.env.PUBLIC_URL + '/';
+
+    return(
+        <div className="Banner">
+            <img></img>
+        </div>
+    )
+}
